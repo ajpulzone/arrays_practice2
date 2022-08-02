f780768f982cb344d70b3bb7ff1e0d97d2ca7323
Annie Pulzone <ajpulzone@gmail.com>
2022-08-02 06:07:45 -0700

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,11 @@
+#String Array
+patients = ["Drake", "Derby", "Fannie", "Tetris"]
+
+#Integer Array
+patient_age = [4, 14, 7, 2]
+
+#Float Array
+patient_weight = [50.2, 12.1, 125.3, 115.4]
+
+#Boolean Array
+patient_good = [true, true, true, false]
